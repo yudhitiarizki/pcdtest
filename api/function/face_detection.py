7951c5e5190a6a59ff2f72afce07c69f7d4b8bec
Yudhitia Rizky <yudhitiarizky2002@gmail.com>
2023-03-12 15:21:21 +0700

    test

--- api/function/face_detection.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-
-def detect_faces(image):
-    face_cascade = cv2.CascadeClassifier('https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5)
-    return faces
