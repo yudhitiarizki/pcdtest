7ed2062dcb27b521e573c61842d3ee17084a4d93
Yudhitia Rizky <yudhitiarizky2002@gmail.com>
2023-03-12 16:15:41 +0700

    test

--- api/function/face_detection.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-def detect_faces(image):
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5)
-    return faces
--- api/index.py
+++ api/index.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, send_file
 import cv2
 import os
 import uuid
-from function.image_blur import blur_faces
 import numpy as np
 
 app = Flask(__name__)
@@ -17,22 +16,31 @@ def up():
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    # read image file
     image_file = request.files['image']
     ext = os.path.splitext(image_file.filename)[1]
     image = cv2.imdecode(np.fromstring(image_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
 
-    # Blur faces in the image
-    image_blurred = blur_faces(image)
+    # detect faces
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5)
 
-    # Generate a unique filename with the original file extension
+    # blur faces in the image
+    for (x, y, w, h) in faces:
+        face_roi = image[y:y+h, x:x+w]
+        face_roi_blur = cv2.GaussianBlur(face_roi, (51, 51), 33)
+        image[y:y+h, x:x+w] = face_roi_blur
+
+    # generate a unique filename with the original file extension
     filename = str(uuid.uuid4()) + ext
     filepath = os.path.join('./static/images/', filename)
 
-    # Save the blurred image to a file
-    cv2.imwrite(filepath, image_blurred)
+    # save the blurred image to a file
+    cv2.imwrite(filepath, image)
 
-    # Return the name of the blurred image file
+    # return the name of the blurred image file
     return render_template('result.html', filename=filename)
+
 
 @app.route('/download/<filename>')
 def download(filename):
